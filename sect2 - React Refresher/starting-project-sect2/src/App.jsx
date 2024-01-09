ef72af6390fbcb16640f9eae6b4412347788dff3
Jhordan Tejada <jhordantej@gmail.com>
2024-01-09 11:42:54 -0600

    Section 2, Vid 16 - Understanding How React Works
    Learned how the different files of a react website, interact to create the website that is shown to the user.

--- sect2 - React Refresher/starting-project-sect2/src/App.jsx
+++ sect2 - React Refresher/starting-project-sect2/src/App.jsx
@@ -1,5 +1,14 @@
+// Section 2, vid 16: Understanding how React Works.
+
+// Define a functional component named App.
+// Functional components are JavaScript functions that return React elements.
 function App() {
+  // The component returns a JSX expression.
+  // JSX is a syntax extension for JavaScript, which looks similar to HTML.
+  // It gets compiled to React.createElement() calls which return React elements.
   return <h1>Hello World!</h1>;
 }
 
+// Export the App component as the default export.
+// This allows other files to import this component using `import App from './App'`.
 export default App;
--- sect2 - React Refresher/starting-project-sect2/src/main.jsx
+++ sect2 - React Refresher/starting-project-sect2/src/main.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+// Section 2, vid 16: Understanding how React Works.
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+// Import the React library. This is necessary to use JSX and React components.
+import React from "react";
+
+// Import the ReactDOM library. This is used to render React components to the DOM.
+import ReactDOM from "react-dom/client";
+
+// Import the App component from the App.js file. This is the main component of your application.
+import App from "./App";
+
+// Import the CSS for your application from index.css.
+import "./index.css";
+
+// Use ReactDOM to render your App component to the DOM.
+// The createRoot method is a part of the new concurrent mode in React. It creates a root for your application on the element with the id 'root'.
+// The render method then renders a React element into the root DOM node.
+ReactDOM.createRoot(document.getElementById("root")).render(
+  // React.StrictMode is a wrapper component that checks for potential problems in your application during development.
   <React.StrictMode>
     <App />
   </React.StrictMode>
-)
+);
