794f7b418f4f178afa41c29ff8992bc700ce2ad3
Jhordan Tejada <jhordantej@gmail.com>
2024-01-09 00:22:56 -0600

    Section 1, Vid 6: Editing The First App
    Learned how to add new pages and edit them in

---
+++ sect1- Getting Started/starting-project/01-starting-project/app/awesome/page.js
@@ -0,0 +1,16 @@
+// Section 1, vid 6: Editting The First App
+
+// This process basically sets up a new page in Next.JS
+// Import the React library (implicitly done in Next.js)
+
+// Define a new functional component called AwesomePage
+export default function AwesomePage() {
+  // The component returns some JSX to render
+  return (
+    // The main HTML element is used to wrap the main content of the document
+    <main>
+      // An h1 heading is used for the page title
+      <h1>NextJS Is an Awesome Page!</h1>
+    </main>
+  );
+}
