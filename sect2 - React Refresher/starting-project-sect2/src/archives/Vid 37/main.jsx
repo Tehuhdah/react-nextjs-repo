684833bfcb91e4a141df9f9fdea511595bbde663
Jhordan Tejada <jhordantej@gmail.com>
2024-01-11 10:59:53 -0600

    Section 2, Vid 37 - Handling Loading State
    Added a new useState object that we can use to check if a fetch is taking place. Modified the app.js in dummy-database to simulate a delay when a fetch request takes place.

---
+++ sect2 - React Refresher/starting-project-sect2/src/archives/Vid 37/main.jsx
@@ -0,0 +1,29 @@
+// Section 2, vid 16: Understanding how React Works.
+
+// Import the React library. This is necessary to use JSX and React components.
+import React from "react";
+
+// Import the ReactDOM library. This is used to render React components to the DOM.
+import ReactDOM from "react-dom/client";
+
+// Import the App component from the App.js file. This is the main component of your application.
+import App from "./App";
+
+// Import the CSS for your application from index.css.
+import "./index.css";
+
+// Use ReactDOM to render your App component to the DOM.
+// The createRoot method is a part of the new concurrent mode in React. It creates a root for your application on the element with the id 'root'.
+// The render method then renders a React element into the root DOM node.
+ReactDOM.createRoot(document.getElementById("root")).render(
+  // React.StrictMode is a wrapper component that checks for potential problems in your application during development.
+  // The App component and all children components will get rendered here.
+
+  // UNDERSTANDING THE FLOW
+  // Posts component contains props that is used to return jsx code that display the author and body.
+  // PostsList component returns the Posts component stored inside of a ul.
+  // App component is the root component which will be rendered below.
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
