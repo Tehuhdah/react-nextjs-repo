136228841b56ee008859619eb0e5edc47d2d7853
Jhordan Tejada <jhordantej@gmail.com>
2024-01-13 17:10:38 -0600

    Section 3, Vid 91 - Reserved File Names, Custom Components & How to Organize A NextJS Project.
    Learned more about reserved file names, how to create a custom component called Header that stores the header content of the page. This component will then be rerendered inside of app/page.js

--- sect3 - NextJS Essentials (App Router)/starting-project-sect3/app/page.js
+++ sect3 - NextJS Essentials (App Router)/starting-project-sect3/app/page.js
@@ -3,12 +3,15 @@
 // Importing the Link component from 'next/link'
 import Link from "next/link";
 
+// Import the Header component from header.js that is in the components folder.
+import Header from "@/components/header";
+
 export default function Home() {
   console.log("Executing...");
   return (
     <main>
-      <img src="/logo.png" alt="A server surrounded by magic sparkles." />
-      <h1>Welcome to this NextJS Course!</h1>
+      {/* Render the Header component that contains the header content of the page (icon and welcome message.)*/}
+      <Header />
       <p>🔥 Let&apos;s get started! 🔥</p>
       <p>
         {/* Used the Link component to redirect and render about/page.js on the server. */}
---
+++ sect3 - NextJS Essentials (App Router)/starting-project-sect3/components/header.js
@@ -0,0 +1,11 @@
+// Section 3, Vid 91 - Reserved File Names, Custom Components & How to Organize A NextJS Project.
+
+// Create a Header component that will contain the header contents of the page. (icon and welcome message.)
+export default function Header() {
+  return (
+    <>
+      <img src="/logo.png" alt="A server surrounded by magic sparkles." />
+      <h1>Welcome to this NextJS Course!</h1>
+    </>
+  );
+}
