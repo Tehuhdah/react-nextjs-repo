575cd68f83a005872d2044a95076b43297c98e10
Jhordan Tejada <jhordantej@gmail.com>
2024-01-18 08:34:02 -0600

    Section 3, Vid 125 - Working with Server Action Responses & useFormState
    Learn the usage of useFormState to conditionally check if an input is correct or not to output a message based on that condition.

--- sect3 - NextJS Essentials (App Router)/foodies-starting-project/archives/Vid 123/components/meals/meals-forms-submit.js
+++ sect3 - NextJS Essentials (App Router)/foodies-starting-project/archives/Vid 123/components/meals/meals-forms-submit.js
@@ -2,14 +2,26 @@
 
 "use client";
 
+// Import the 'useFormStatus' hook from 'react-dom'
 import { useFormStatus } from "react-dom";
 
+// Define the 'MealsFormSubmit' component
 export default function MealsFormSubmit() {
+  // Call the 'useFormStatus' hook, which returns an object that includes a 'pending' property
+  // This 'pending' property is likely a boolean that indicates whether the form is currently being submitted
   const { pending } = useFormStatus();
 
+  // Render a button
   return (
+    // The 'disabled' prop of the button is set to the value of 'pending'
+    // This means the button will be disabled while the form is being submitted
     <button disabled={pending}>
-      {pending ? "Submitting..." : "Share Meal"}
+      {
+        // The text of the button depends on whether the form is being submitted
+        // If 'pending' is true, the text is "Submitting..."
+        // If 'pending' is false, the text is "Share Meal"
+        pending ? "Submitting..." : "Share Meal"
+      }
     </button>
   );
 }
