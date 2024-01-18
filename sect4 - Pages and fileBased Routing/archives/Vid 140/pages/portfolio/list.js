e51fb1ee89e49173c39bf7cc86a661600cbd2904
Jhordan Tejada <jhordantej@gmail.com>
2024-01-18 11:50:40 -0600

    Section 4, Vid 140 - Working with Nested Paths & Routes
    Created a nested route called portfolio inside of the pages folder. This will be our portfolio page when the user gets redirected to /portfolio route. Created a list page inside of the portfolio route. This will be our list page when the user gets redirected to /portfolio/list.

---
+++ sect4 - Pages and fileBased Routing/archives/Vid 140/pages/portfolio/list.js
@@ -0,0 +1,12 @@
+// Section 4, Vid 140 - Working with Nested Paths & Routes
+
+// Creating a list page for the portfolio route ('/portfolio/list)
+function ListPage() {
+  return (
+    <div>
+      <h1>The List Page</h1>
+    </div>
+  );
+}
+
+export default PortfolioPage;
