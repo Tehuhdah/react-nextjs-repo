cf69121e8c33e685a842b5a0431e62b695e048e1
Jhordan Tejada <jhordantej@gmail.com>
2024-01-18 13:01:35 -0600

    Section 4, Vid Extracting Dynamic Path Segment data (Dynamic Routes)
    Used the useRouter hook to get access to various data properties through the router object.

---
+++ sect4 - Pages and fileBased Routing/archives/Vid 142/pages/about.js
@@ -0,0 +1,12 @@
+// Section 4, Vid 139 - Addiing a Named or Static Route File
+
+// Creating an about page of the project which will be the home page. ('/about')
+function AboutPage() {
+  return (
+    <div>
+      <h1>The About Page</h1>
+    </div>
+  );
+}
+
+export default AboutPage;
