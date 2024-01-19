1f21406b1d9c7512fb04355501b642b00bfc1549
Jhordan Tejada <jhordantej@gmail.com>
2024-01-19 08:50:00 -0600

    Section 5 Setup
    Created the Section 5 Project where we added the main pages of the projects with dummy text.

---
+++ sect5 - Working with fileBased Routing/archives/Vid 153/pages/events/[eventId].js
@@ -0,0 +1,12 @@
+// Section 5, Vid 151 - Setting Up the Main Pages.
+
+// The Event Detail Page of the project. The user will get redirected to this page if the url is '/event/...'
+function EventDetailPage() {
+  return (
+    <div>
+      <h1>Event Detail</h1>
+    </div>
+  );
+}
+
+export default EventDetailPage;
---
+++ sect5 - Working with fileBased Routing/archives/Vid 153/pages/index.js
@@ -0,0 +1,12 @@
+// Section 5, Vid 151 - Setting Up the Main Pages.
+
+// The Home Page of the project. The user will get redirected to this page if the url is '/'
+function HomePage() {
+  return (
+    <div>
+      <h1>The Home Page</h1>
+    </div>
+  );
+}
+
+export default HomePage;
