e1b24030f8e511cc3feb733bb94873f8b33aec51
Jhordan Tejada <jhordantej@gmail.com>
2024-01-19 09:58:17 -0600

    Section 5, Vid 157 - Adding More React Components & Connecting Components
    Added 2 event components that will use the getFeaturedEvents function from dummy-data.js to populate the events to our home page.

---
+++ sect5 - Working with fileBased Routing/archives/Vid 157/pages copy/events/index.js
@@ -0,0 +1,12 @@
+// Section 5, Vid 151 - Setting Up the Main Pages.
+
+// The Events Page of the project. The user will get redirected to this page if the url is '/event'
+function AllEventsPage() {
+  return (
+    <div>
+      <h1>All Events</h1>
+    </div>
+  );
+}
+
+export default AllEventsPage;
