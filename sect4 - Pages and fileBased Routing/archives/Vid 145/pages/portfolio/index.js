99778786c067cf91213f9395d3d78dc4a3abf662
Jhordan Tejada <jhordantej@gmail.com>
2024-01-18 18:59:03 -0600

    Section 4, Vid 145 - Navigating with the 'Link' Component
    Learned how to use the Link component to redirect the user to different pages.

---
+++ sect4 - Pages and fileBased Routing/archives/Vid 145/pages/portfolio/index.js
@@ -0,0 +1,12 @@
+// Section 4, Vid 140 - Working with Nested Paths & Routes
+
+// Creating an index page for the portfolio route ('/portfolio)
+function PortfolioPage() {
+  return (
+    <div>
+      <h1>The Portfolio Page</h1>
+    </div>
+  );
+}
+
+export default PortfolioPage;
