c264ae47bf021c0f7e5113915be73d22bfee1b9d
Jhordan Tejada <jhordantej@gmail.com>
2024-01-23 10:48:29 -0600

    Section 6, Vid 191 - Dynamic Pages & 'getServerSideProps'
    Learned more about getServerSideProps and how can we can use this special function for dynamic pages.

---
+++ sect6 - Page PreRendering DataFetching/archives/Vid 191/pages/user-profile.js
@@ -0,0 +1,25 @@
+// Section 6, Vid 189 - Using 'getServerSideProps' for Server-side Rendering
+
+function UserProfilePage(props) {
+  return <h1>{props.username}</h1>;
+}
+
+export default UserProfilePage;
+
+// This is a special function that Next.js calls when a request is made to the page that this file represents
+// It runs on the server-side for every request, and its return value determines the props for the page component
+export async function getServerSideProps(context) {
+  // The 'context' object provided by Next.js in getServerSideProps or getStaticProps contains several useful properties
+  // 'params' contains the route parameters for pages using dynamic routes
+  // 'req' is the HTTP IncomingMessage object for the request
+  // 'res' is the HTTP response object
+  const { params, req, res } = context;
+
+  // The 'props' property is an object that will be passed as props to the page component
+  // In this case, the 'username' prop is set to the string "Jhordan"
+  return {
+    props: {
+      username: "Jhordan",
+    },
+  };
+}
