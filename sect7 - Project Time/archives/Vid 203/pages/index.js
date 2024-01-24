5aa3da3a7f8ebfb5383eb3ee1148a811bf86119c
Jhordan Tejada <jhordantej@gmail.com>
2024-01-24 11:05:01 -0600

    Section 7, Vid 203 - Loading Data & Paths for Dynamic Pages
    Implemented Static Site Generation (SSG) on the events detail page to improve performance and SEO by pre-rendering the events data using getStaticProps and getStaticPaths due this page being dynamic.

---
+++ sect7 - Project Time/archives/Vid 203/pages/index.js
@@ -0,0 +1,35 @@
+// Section 5, Vid 151 - Setting Up the Main Pages.
+
+// Updated
+// Section 7, Vid 202 - Adding Static Site Generation (SSG) On The Home Page
+
+import EventList from "../components/events/event-list";
+
+// Importing the getFeaturedEvents function from api-util.js
+import { getFeaturedEvents } from "../helpers/api-util";
+
+// The Home Page of the project. The user will get redirected to this page if the url is '/'
+function HomePage(props) {
+  return (
+    <div>
+      {/* Rendering the EventList component and passing the events prop to it. 
+          The events prop contains the featured events data fetched from the API. */}
+      <EventList items={props.events} />
+    </div>
+  );
+}
+
+// getStaticProps is a Next.js function that runs at build time in production,
+// and generates the props for the page, in this case, the featured events data
+export async function getStaticProps() {
+  // Fetching the featured events data from the API
+  const featuredEvents = await getFeaturedEvents();
+  // Returning the featured events data as props for the page
+  return {
+    props: {
+      events: featuredEvents,
+    },
+  };
+}
+
+export default HomePage;
